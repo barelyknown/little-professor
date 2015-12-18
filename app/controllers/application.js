dd6ecee00a6c7bb36c13819a67bd9bca919b7367
Sean Devine <barelyknown@icloud.com>
2015-12-18 11:40:34 -0500

    add SET and GO buttons and move actions to application controller

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,14 +1,52 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  levels: [1,2,3,4],
+
   isOn: null,
 
   level: null,
 
+  mode: null,
+
   actions: {
+    turnOn() {
+      this.set('isOn', true);
+      this.set('level', this.get('levels').objectAt(0));
+      this.set('operator', '+');
+      this.set('mode', 'settings');
+    },
+
+    turnOff() {
+      this.set('isOn', false);
+    },
+
     changeOperator(operator) {
-      if (this.get('isOn')) {
+      if (this.get('isOn') && this.get('mode') === 'settings') {
         this.set('operator', operator);
+      }
+    },
+
+    playGame() {
+      if (this.get('isOn') && this.get('mode') === 'settings') {
+        this.set('mode', 'game');
+      }
+    },
+
+    changeSettings() {
+      if (this.get('isOn') && this.get('mode') === 'game') {
+        this.set('mode', 'settings');
+      }
+    },
+
+    nextLevel() {
+      if (this.get('isOn') && this.get('mode') === 'settings') {
+        const level = this.get('level');
+        const levelIndex = this.get('levels').indexOf(level);
+        const nextLevel = this.get('levels').objectAt(
+          (levelIndex + 1) % this.get('levels').length
+        );
+        this.set('level', nextLevel);
       }
     }
   }
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,33 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  levels: [1,2,3,4],
-
   setupController(controller) {
     controller.set('isOn', false);
-  },
-
-  actions: {
-    turnOn() {
-      const c = this.controllerFor('application');
-      c.set('isOn', true);
-      c.set('level', this.get('levels').objectAt(0));
-      c.set('operator', '+');
-    },
-    turnOff() {
-      const c = this.controllerFor('application');
-      c.set('isOn', false);
-    },
-    nextLevel() {
-      const c = this.controllerFor('application');
-      if (c.get('isOn')) {
-        const level = c.get('level');
-        const levelIndex = this.get('levels').indexOf(level);
-        const nextLevel = this.get('levels').objectAt(
-          (levelIndex + 1) % this.get('levels').length
-        );
-        c.set('level', nextLevel);
-      }
-    }
   }
 });
