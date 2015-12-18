107fad3fa38a97485ad89e32a8699ca40b532ca5
Sean Devine <barelyknown@icloud.com>
2015-12-18 16:55:22 -0500

    use division instead of modulo

--- app/helpers/format-operator.js
+++ app/helpers/format-operator.js
@@ -12,7 +12,7 @@ export function formatOperator(params/*, hash*/) {
     case "*":
       formatted = "&times;";
       break;
-    case "%":
+    case "/":
       formatted = "&divide;";
       break;
   }
